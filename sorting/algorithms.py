d4e1a1b64ce724f42d9c557291eca9e1ef97ff1b
Agustin Borgna <hello@aborgna.com.ar>
2016-06-01 19:21:08 -0300

    heapsort

--- sorting/algorithms.py
+++ sorting/algorithms.py
@@ -19,7 +19,81 @@ def quicksort(a):
 
 @presentar
 def heapsort(a):
+    length = len(a)
+
+    heapify(a, length)
+
+    heapLength = length
+    oldRoot = a[0] if length else None
+    for i in range(length-1, 0, -1):
+        newRoot = a[i]
+
+        a[0] = newRoot
+        a[i] = oldRoot
+
+        heapLength -= 1
+
+        oldRoot = heapBalance(a,0,heapLength,newRoot)
+
     return a
+
+def heapify(a, length):
+
+    for i in range(((int)(length/2))-1,-1,-1):
+        heapBalance(a,i,length)
+
+def heapBalance(a, i, length, eRoot=None, eLeft=None, eRight=None):
+    eRoot, eLeft, eRight = heapElems(a,i,length,eRoot,eLeft,eRight)
+
+    newRoot = eRoot
+
+    maxElem = 'root'
+
+    if eLeft is not None and eRoot < eLeft:
+        maxElem = 'left'
+        if eRight is not None and eLeft < eRight:
+            maxElem = 'right'
+    elif eRight is not None and eRoot < eRight:
+        maxElem = 'right'
+
+    if maxElem == 'left':
+        a[i] = eLeft
+        a[heapChildL(i)] = eRoot
+
+        heapBalance(a, heapChildL(i), length, eRoot, None, None)
+
+        newRoot = eLeft
+
+    elif maxElem == 'right':
+        a[i] = eRight
+        a[heapChildR(i)] = eRoot
+
+        heapBalance(a, heapChildR(i), length, eRoot, None, None)
+
+        newRoot = eRight
+
+    return newRoot
+
+def heapChildL(i):
+    return i * 2 + 1
+
+def heapChildR(i):
+    return i * 2 + 2
+
+def heapParent(i):
+    return (int) ((i - 1) / 2)
+
+def heapElems(a, i, length, eRoot=None, eLeft=None, eRight=None):
+    if eRoot is None:
+        eRoot = a[i]
+
+    if eLeft is None and heapChildL(i) < length:
+        eLeft = a[heapChildL(i)]
+
+    if eRight is None and heapChildR(i) < length:
+        eRight = a[heapChildR(i)]
+
+    return eRoot, eLeft, eRight
 
 @presentar
 def mergesort(a):
@@ -33,7 +107,7 @@ def mergesort(a):
     l = mergesort(a[:middle])
     r = mergesort(a[middle:])
 
-    # Conquer
+    # Conquer (merge)
     i = 0
     left_i = 0
     right_i = 0
