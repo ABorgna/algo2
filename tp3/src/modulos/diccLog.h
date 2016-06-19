e017698ac20d6350ab2943af354f086f1141272e
Agustin Borgna <hello@aborgna.com.ar>
2016-06-19 19:10:42 -0300

    interfaces de los diccionarios

--- tp3/src/modulos/diccLog.h
+++ tp3/src/modulos/diccLog.h
@@ -10,6 +10,46 @@ namespace tp3 {
 
     template<class K, class T>
     class DiccLog {
+        public:
+
+            struct ClaveValor {
+                const K& clave;
+                const T& significado;
+
+                ClaveValor(const K& c, const T& s) :
+                    clave(c), significado(s) {};
+            };
+
+            DiccLog();
+
+            void definir(const K& k, const T& v);
+            bool definido(const K& k) const;
+
+            const T& obtener(const K& k) const;
+            T& obtener(const K& k);
+
+            void borrar(const K& k);
+
+            DiccLog<K,T>::ClaveValor maximo();
+            DiccLog<K,T>::ClaveValor minimo();
+
+        private:
+
+            struct Nodo {
+                K clave;
+                T valor;
+
+                Nodo* menor;
+                Nodo* mayor;
+                Nodo* padre;
+
+                unsigned int fdb;
+            };
+
+            Nodo* raiz_;
+            Nodo* minimo_;
+            Nodo* maximo_;
+
     };
 
 }
--- tp3/src/modulos/diccTrie.h
+++ tp3/src/modulos/diccTrie.h
@@ -10,6 +10,40 @@ namespace tp3 {
 
     template<class T>
     class DiccTrie {
+        public:
+
+            struct ClaveValor {
+                const std::string& clave;
+                const T& significado;
+
+                ClaveValor(const std::string& c, const T& s) :
+                    clave(c), significado(s) {};
+            };
+
+            DiccTrie();
+
+            void definir(const std::string& k, const T& v);
+            bool definido(const std::string& k) const;
+
+            const T& obtener(const std::string& k) const;
+            T& obtener(const std::string& k);
+
+            void borrar(const std::string& k);
+
+            DiccTrie<T>::ClaveValor maximo();
+            DiccTrie<T>::ClaveValor minimo();
+
+        private:
+
+            struct Nodo {
+                T valor;
+                Nodo* hijos[256];
+            };
+
+            Nodo* raiz_;
+            ClaveValor minimo_;
+            ClaveValor maximo_;
+
     };
 
 }
